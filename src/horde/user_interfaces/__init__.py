ee61aad6c4ea10a6e203f45dc319abc0d7e01db7
boonhapus <me@boonhapus>
2023-01-02 15:07:10 -0600

    imports pls

--- src/horde/user_interfaces/__init__.py
+++ src/horde/user_interfaces/__init__.py
@@ -1,2 +1,2 @@
-from horde.user_interfaces.terminal import TerminalUI
-from horde.user_interfaces.printer import PrinterUI
+# from horde.user_interfaces.terminal import TerminalUI
+# from horde.user_interfaces.printer import PrinterUI
